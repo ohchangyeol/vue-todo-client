2fc0ffe1fe5951d8f32a9754122337ed938e2a5d
cy950315 <cy950315@naver.com>
2024-05-23 11:33:58 +0900

    레파지토리 분리 작업

---
+++ src/components/contents/todo/TodoCard.vue
@@ -0,0 +1,17 @@
+<script setup>
+import TodoItem from './TodoItem.vue'
+
+  const props = defineProps({
+    todos : Array
+  })
+  
+</script>
+
+<!-- ========= template start ========= -->
+<template>
+  <v-card v-if="todos.length > 0">
+    <!-- <v-slide-x-transition class="py-0" group tag="v-list"> -->
+        <TodoItem :todo ='todo' :idx='i' v-for="(todo, i) in todos" :key="`${todo.id}-${todo.text}`" />
+     <!-- </v-slide-x-transition> -->
+  </v-card>
+</template>
